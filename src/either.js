0e7ad6e56d782f596f9143aabd16ea7f6dce444f
Vincent Driessen <me@nvie.com>
2017-10-21 10:14:25 +0200

    Update either() implementation

--- src/either.js
+++ src/either.js
@@ -6,16 +6,19 @@ import { makeErr } from './asserts';
 import type { Verifier } from './types';
 
 export function either<T1, T2>(v1: Verifier<T1>, v2: Verifier<T2>): Verifier<T1 | T2> {
-    return (blob: any) => {
-        return v1(blob).dispatch(
-            value => Ok(value),
-            err1 => {
-                const r2 = v2(blob);
-                return r2.dispatch(
-                    value => Ok(value),
-                    err2 => makeErr('Error 1:\n' + err1.message + '\n\n' + 'Error 2:\n' + err2.message)
-                );
-            }
+    return (blob: any) =>
+        v1(blob).dispatch(
+            value1 => Ok(value1),
+            err1 =>
+                v2(blob).dispatch(
+                    value2 => Ok(value2),
+                    err2 =>
+                        makeErr(
+                            'None of the allowed alternatives matched',
+                            "I've tried to match the following alternatives in order, but none of them could decode the input.",
+                            blob,
+                            [err1, err2]
+                        )
+                )
         );
-    };
 }
