5e77472a8723dcd72d6360a613d4a54778b11e88
Vincent Driessen <me@nvie.com>
2017-10-21 16:10:51 +0200

    Expand line coverage for tuple tests

--- src/__tests__/tuple.test.js
+++ src/__tests__/tuple.test.js
@@ -8,7 +8,16 @@ describe('tuples', () => {
     it('tuple2', () => {
         const verifier = tuple2(string, number);
         expect(verifier(['foo', 42]).unwrap()).toEqual(['foo', 42]);
+        expect(verifier(['foo', 'bar']).isErr()).toBe(true);
         expect(verifier([42, 'foo']).isErr()).toBe(true);
-        expect(verifier([42, 'foo', true]).isErr()).toBe(true);
+
+        // Invalid
+        expect(verifier('not an array').isErr()).toBe(true);
+        expect(verifier(undefined).isErr()).toBe(true);
+
+        // Wrong arity (not a 2-tuple)
+        expect(verifier([]).isErr()).toBe(true);
+        expect(verifier(['foo']).isErr()).toBe(true);
+        expect(verifier(['foo', 42, true]).isErr()).toBe(true);
     });
 });
