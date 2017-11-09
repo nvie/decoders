39d968457c8d8ed5b8df3a9e79d55baab0a3da5b
Vincent Driessen <me@nvie.com>
2017-11-09 20:11:11 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
  * Why? All JSON responses coming from our API endpoints are just that: free-form
  * JSON data.  To Flow, the only type classification possilbe is "any" -- effectively
  * turning off all type checks for anything related to JSON.  To the receiving end
- * (our frontend), the structure of that data is completely opaque to any type 
+ * (our frontend), the structure of that data is completely opaque to any type
  * checkers since JSON values can be anything: an object, an array, null, a string,
  * a bool, etc.  Our type system is not a runtime type system, so we need a way of
  * "converting" an any-type JSON value into a type that we want to work with in our
