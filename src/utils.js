38988153a3a3a7da18dcd38c4a3735e74d6095a7
Vincent Driessen <me@nvie.com>
2017-12-10 21:47:18 +0100

    Document utility helper functions

--- src/utils.js
+++ src/utils.js
@@ -5,14 +5,32 @@ import { Ok } from 'lemons';
 import { makeErr } from './error';
 import type { Decoder } from './types';
 
+/**
+ * Given a decoder T and a mapping function from T's to V's, returns a decoder
+ * for V's.  This is useful to change the original input data.
+ */
 export function map<T, V>(decoder: Decoder<T>, mapper: T => V): Decoder<V> {
     return compose(decoder, x => Ok(mapper(x)));
 }
 
+/**
+ * Compose two decoders by passing the result of the first into the second.
+ * The second decoder may assume as its input type the output type of the first
+ * decoder (so it's not necessary to accept the typical "any").  This is useful
+ * for "narrowing down" the checks.  For example, if you want to write
+ * a decoder for positive numbers, you can compose it from an existing decoder
+ * for any number, and a decoder that, assuming a number, checks if it's
+ * positive.  Very often combined with the predicate() helper as the second
+ * argument.
+ */
 export function compose<T, V>(decoder: Decoder<T>, next: Decoder<V, T>): Decoder<V> {
     return (blob: any) => decoder(blob).andThen(next);
 }
 
+/**
+ * Factory function returning a Decoder<T>, given a predicate function that
+ * accepts/rejects the input of type T.
+ */
 export function predicate<T>(predicate: T => boolean, msg: string): Decoder<T> {
     return (value: T) => {
         return predicate(value) ? Ok(value) : makeErr(msg, value, []);
