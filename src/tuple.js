67d914c18af4b608d8ced63df6bfd5f5e61f0ad5
Vincent Driessen <me@nvie.com>
2017-12-10 22:41:22 +0100

    Make tuple2 decoder lazier

--- src/tuple.js
+++ src/tuple.js
@@ -6,10 +6,10 @@ import { makeErr } from './error';
 import type { Decoder } from './types';
 
 /**
- * Builds a Decoder that returns Ok for 2-tuples of `[T, V]`, given Verifiers
- * for `T` and `V`.  Err otherwise.
+ * Builds a Decoder that returns Ok for 2-tuples of [T1, T2], given Decoders
+ * for T1 and T2.  Err otherwise.
  */
-export function tuple2<T, V>(verifier1: Decoder<T>, verifier2: Decoder<V>): Decoder<[T, V]> {
+export function tuple2<T1, T2>(decoder1: Decoder<T1>, decoder2: Decoder<T2>): Decoder<[T1, T2]> {
     return (blobs: any) => {
         if (!Array.isArray(blobs)) {
             return makeErr('Must be an array', blobs, []);
@@ -21,15 +21,15 @@ export function tuple2<T, V>(verifier1: Decoder<T>, verifier2: Decoder<V>): Deco
 
         const [blob1, blob2] = blobs;
 
-        const t1 = verifier1(blob1);
-        const t2 = verifier2(blob2);
-
+        const t1 = decoder1(blob1);
         return t1.dispatch(
-            (value1: T) =>
-                t2.dispatch(
-                    (value2: V) => Ok([value1, value2]),
+            (value1: T1) => {
+                const t2 = decoder2(blob2);
+                return t2.dispatch(
+                    (value2: T2) => Ok([value1, value2]),
                     e => makeErr('Unexpected value in 2nd tuple position', blob2, [e])
-                ),
+                );
+            },
             e => makeErr('Unexpected value in 1st tuple position', blob1, [e])
         );
     };
