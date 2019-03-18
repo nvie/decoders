87fe59b991a747647bbb7256c5f33144ef84b753
Vincent Driessen <me@nvie.com>
2019-03-18 09:48:32 +0100

    Fix type of fail() decoder

--- src/fail.js
+++ src/fail.js
@@ -8,6 +8,6 @@ import type { Decoder } from './types';
 /**
  * Decoder that always fails with the given error message, no matter what the input.
  */
-export function fail<T>(msg: string): Decoder<T> {
+export function fail(msg: string): Decoder<empty> {
     return (blob: mixed) => Err(annotate(blob, msg));
 }
