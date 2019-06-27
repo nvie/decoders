864e39bb83bf8b31f10d1b8e7e8349b57469bcb3
Jessica <jessicamfranco3@gmail.com>
2019-06-27 15:53:24 +0900

    Add support for type predicates in predicate()

--- src/types/utils-tests.ts
+++ src/types/utils-tests.ts
@@ -8,3 +8,14 @@ compose(
     string,
     predicate(s => s.startsWith('x'), 'Must start with x')
 );
+
+const a = predicate((foo): foo is string => typeof foo === 'string', 'Is string');
+// $ExpectType Decoder<string, unknown>
+a;
+
+const b = predicate((foo: string): foo is 'a'|'b' => foo === 'a' || foo === 'b', 'Is a or b');
+// $ExpectType Decoder<"a" | "b", string>
+b;
+
+// $ExpectType Decoder<"a" | "b", unknown>
+compose(a, b);
