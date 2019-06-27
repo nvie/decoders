b32bde7661475576e86656cb95308260096588c1
Jessica <jessicamfranco3@gmail.com>
2019-06-27 15:37:59 +0900

    Update instanceOf to support constructors that have required arguments

--- src/types/instanceOf-tests.ts
+++ src/types/instanceOf-tests.ts
@@ -5,3 +5,12 @@ instanceOf(Error);
 
 // $ExpectType Decoder<TypeError, unknown>
 instanceOf(TypeError);
+
+// $ExpectType Decoder<RegExp, unknown>
+instanceOf(RegExp);
+
+// $ExpectType Decoder<Promise<string>, unknown>
+instanceOf<Promise<string>>(Promise);
+
+// $ExpectError
+instanceOf<Promise<string>>(Set);
