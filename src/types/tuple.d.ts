8ec1401d52925ec1de5e6690ead38e1255d2af7b
Vincent Driessen <me@nvie.com>
2020-05-15 10:46:10 +0200

    Add typescript definitions for tuple1, too

--- src/types/tuple.d.ts
+++ src/types/tuple.d.ts
@@ -1,5 +1,6 @@
 import { Decoder } from './types';
 
+export function tuple1<T1>(d1: Decoder<T1>): Decoder<[T1]>;
 export function tuple2<T1, T2>(d1: Decoder<T1>, d2: Decoder<T2>): Decoder<[T1, T2]>;
 export function tuple3<T1, T2, T3>(d1: Decoder<T1>, d2: Decoder<T2>, d3: Decoder<T3>): Decoder<[T1, T2, T3]>;
 export function tuple4<T1, T2, T3, T4>(d1: Decoder<T1>, d2: Decoder<T2>, d3: Decoder<T3>, d4: Decoder<T4>): Decoder<[T1, T2, T3, T4]>;
