c3c15775f79849aa6d2cd63af78b3fe636787c5c
Vincent Driessen <me@nvie.com>
2020-05-25 12:59:56 +0200

    Add test cases for self-referential inputs

---
+++ src/__tests__/circular-refs.test.js
@@ -0,0 +1,52 @@
+// @flow strict
+
+import { guard } from '../guard';
+import { number } from '../number';
+import { exact, object, pojo } from '../object';
+import { mixed } from '../constants';
+import { optional } from '../optional';
+import { string } from '../string';
+
+describe('objects w/ circular refs', () => {
+    // Take any decoder and pass in some self-referential object
+    const value = { foo: 42 };
+    const self = value;
+    // $FlowFixMe - let's create a self-referential object
+    value.self = self;
+    it('valid', () => {
+        expect(guard(object({ foo: number }))(value)).toEqual({ foo: 42 });
+        expect(guard(object({ foo: number, self: mixed }))(value)).toEqual({ foo: 42, self });
+        expect(guard(object({ foo: number, self: pojo }))(value)).toEqual({ foo: 42, self });
+        expect(guard(object({ foo: number, self: object({ foo: number }) }))(value)).toEqual({
+            foo: 42,
+            self: { foo: 42 },
+        });
+        expect(
+            guard(
+                object({ foo: number, self: object({ foo: number, self: object({ self: object({ foo: number }) }) }) })
+            )(value)
+        ).toEqual({
+            foo: 42,
+            self: {
+                foo: 42,
+                self: {
+                    self: {
+                        foo: 42,
+                    },
+                },
+            },
+        });
+    });
+
+    it('invalid', () => {
+        expect(object({ foo: string })(value).isErr()).toBe(true);
+        expect(object({ foo: string, self: mixed })(value).isErr()).toBe(true);
+        expect(object({ foo: string, self: pojo })(value).isErr()).toBe(true);
+        expect(object({ foo: number, self: object({ foo: string }) })(value).isErr()).toBe(true);
+        expect(
+            object({ foo: number, self: object({ foo: number, self: object({ self: object({ foo: string }) }) }) })(
+                value
+            ).isErr()
+        ).toBe(true);
+    });
+});
