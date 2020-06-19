13797e7d55aa5bb6ef3751bd328b396876073501
Vincent Driessen <me@nvie.com>
2020-06-19 17:26:52 +0200

    Improve inferred type for object() decoders
    This is to let TypeScript infer the type for an expression like
    
        object({ a: optional(number) })
    
    as
    
        { a?: number }
    
    Instead of
    
        { a: number | undefined }
    
    In practice, it means that objects with a missing `a` key (i.e. an
    implicit-undefined) will not be rejected by the TypeScript compiler any
    longer.

--- src/types/object-tests.ts
+++ src/types/object-tests.ts
@@ -1,14 +1,14 @@
-import { exact, object, pojo, string } from 'decoders';
+import { exact, object, optional, pojo, string } from 'decoders';
 
-// $ExpectType Decoder<{ foo: string; bar: { qux: string; }; }, unknown>
+// $ExpectType Decoder<{ bar: { qux: string; }; foo?: string | undefined; }, unknown>
 object({
-    foo: string,
+    foo: optional(string),
     bar: object({ qux: string }),
 });
 
-// $ExpectType Decoder<{ foo: string; bar: { qux: string; }; }, unknown>
+// $ExpectType Decoder<{ bar: { qux: string; }; foo?: string | undefined; }, unknown>
 exact({
-    foo: string,
+    foo: optional(string),
     bar: object({ qux: string }),
 });
 
--- src/types/object.d.ts
+++ src/types/object.d.ts
@@ -1,17 +1,25 @@
 import { $DecoderType, Decoder } from './types';
+import { AllowImplicit } from './helpers';
+
+export type ObjectDecoderType<T> = AllowImplicit<
+    { [key in keyof T]: $DecoderType<T[key]> }
+>;
 
 export const pojo: Decoder<{ [key: string]: unknown }>;
+
 export function object<O extends { [key: string]: Decoder<any> }>(
     mapping: O
-): Decoder<
-    {
-        [key in keyof O]: $DecoderType<O[key]>;
-    }
->;
+): Decoder<{ [K in keyof ObjectDecoderType<O>]: ObjectDecoderType<O>[K] }>;
+//         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//         This is basically just equivalent to:
+//             ObjectDecoderType<O>
+//
+//         But by "resolving" this with a mapped type, we remove the helper
+//         type names from the inferred type here, making this much easier to
+//         work with while developing.
+
 export function exact<O extends { [key: string]: Decoder<any> }>(
     mapping: O
-): Decoder<
-    {
-        [key in keyof O]: $DecoderType<O[key]>;
-    }
->;
+): Decoder<{ [K in keyof ObjectDecoderType<O>]: ObjectDecoderType<O>[K] }>;
+//         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//         Ditto (see above)
