7ef6b59c507fc360ea199b76c8d55ddd2bb98da2
pratiknaik-tavisca <52398053+pratiknaik-tavisca@users.noreply.github.com>
2020-09-15 15:42:16 +0530

    nonEmptyArray is not exported
    nonEmptyArray function is defined in array.js but is not exported from the type definition.

--- src/types/array.d.ts
+++ src/types/array.d.ts
@@ -2,3 +2,5 @@ import { Decoder } from './types';
 
 export const poja: Decoder<unknown[]>;
 export function array<T>(decoder: Decoder<T>): Decoder<T[]>;
+export function nonEmptyArray<T>(decoder: Decoder<T>): Decoder<T[]>;
+
