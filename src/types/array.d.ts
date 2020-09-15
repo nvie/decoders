52b637bb2afeaed6e6393dc850d5ca8ca66a1b1c
Vincent Driessen <me@nvie.com>
2020-09-15 13:41:13 +0200

    Re-run prettier

--- src/types/array.d.ts
+++ src/types/array.d.ts
@@ -3,4 +3,3 @@ import { Decoder } from './types';
 export const poja: Decoder<unknown[]>;
 export function array<T>(decoder: Decoder<T>): Decoder<T[]>;
 export function nonEmptyArray<T>(decoder: Decoder<T>): Decoder<T[]>;
-
