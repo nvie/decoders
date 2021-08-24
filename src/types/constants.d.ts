4fcdb63387a02c1e904194c9de8f739d2ea610dd
Vincent Driessen <me@nvie.com>
2021-08-24 14:48:52 +0200

    Delete alternatives that aren't needed any longer
    All constants will have to be scalars anyway, so it's good to restrict that and not also allow
    unbounded `T`. But hardcoded values don't need to be, per se.

--- src/types/constants.d.ts
+++ src/types/constants.d.ts
@@ -6,7 +6,6 @@ import { Scalar } from './either';
 export const null_: Decoder<null>;
 export const undefined_: Decoder<undefined>;
 export function constant<T extends Scalar>(value: T): Decoder<T>;
-export function constant<T>(value: T): Decoder<T>;
 export function hardcoded<T extends Scalar>(value: T): Decoder<T>;
 export function hardcoded<T>(value: T): Decoder<T>;
 export const mixed: Decoder<unknown>;
