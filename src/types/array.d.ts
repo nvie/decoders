84178ee8f8d9827c0adada5450a26e69e20601a7
Mariusz Szczepańczyk <mszczepanczyk@gmail.com>
2021-10-14 22:02:20 +0200

    Fix nonEmptyArray's type

--- src/types/array.d.ts
+++ src/types/array.d.ts
@@ -2,4 +2,4 @@ import { Decoder } from './types';
 
 export const poja: Decoder<unknown[]>;
 export function array<T>(decoder: Decoder<T>): Decoder<T[]>;
-export function nonEmptyArray<T>(decoder: Decoder<T>): Decoder<T[]>;
+export function nonEmptyArray<T>(decoder: Decoder<T>): Decoder<[T, ...T[]]>;
