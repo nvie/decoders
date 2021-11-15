fc86fe7b8fd9b4e799d9d5dae4091a54822c7ed4
Vincent Driessen <me@nvie.com>
2021-11-15 21:45:05 +0100

    Remove unneeded exports

--- src/debrief/types.js
+++ src/debrief/types.js
@@ -2,31 +2,34 @@
 
 type cast = $FlowFixMe;
 
-export type ScalarAnnotation = {
+type ScalarAnnotation = {
     type: 'ScalarAnnotation',
     value: mixed,
     annotation?: string,
 };
 
-export type FunctionAnnotation = {
+type FunctionAnnotation = {
     type: 'FunctionAnnotation',
     annotation?: string,
 };
 
+// TODO: Remove this export - it should be an implementation detail!
 export type CircularRefAnnotation = {
     type: 'CircularRefAnnotation',
     annotation?: string,
 };
 
+// TODO: Remove this export - it should be an implementation detail!
 export type AnnPair = { key: string, value: Annotation };
 
+// TODO: Remove this export - it should be an implementation detail!
 export type ObjectAnnotation = {
     type: 'ObjectAnnotation',
     pairs: Array<AnnPair>,
     annotation?: string,
 };
 
-export type ArrayAnnotation = {
+type ArrayAnnotation = {
     type: 'ArrayAnnotation',
     items: Array<Annotation>,
     annotation?: string,
