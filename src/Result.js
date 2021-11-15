23cffba3706657deedb98f965d974f1134bc0208
Vincent Driessen <me@nvie.com>
2021-11-15 21:43:19 +0100

    Strip classes, replace with simpler objects and functions

--- src/Result.js
+++ src/Result.js
@@ -1,140 +1,104 @@
 // @flow strict
 
 /**
- * Result error value
- *     = Ok value
- *     | Err error
+ * Result <value> <error>
+ *     = Ok <value>
+ *     | Err <error>
  */
-
-const Ok = 'Ok';
-const Err = 'Err';
 
 // prettier-ignore
-opaque type $ResultT<E, T> =
-    | {| type: typeof Ok, value: T |}
-    | {| type: typeof Err, error: E |}
+export type Result<T, E> =
+    | {| type: 'ok',  value: T |}
+    | {| type: 'err', error: E |}
 
 /**
- * Represents a union type that's either a legit value or an error:
- *
- *     Result error value
- *         = Ok value
- *         | Err error
- *
+ * Create a new Result instance representing a successful computation.
  */
-class Result<E, T> {
-    _r: $ResultT<E, T>;
+export function Ok<T, E>(value: T): Result<T, E> {
+    return { type: 'ok', value };
+}
 
-    /**
-     * **Do not call this constructor directly!**  Use either `Result.ok()` or
-     * `Result.err()` to construct a new Result instance.
-     */
-    constructor(r: $ResultT<E, T>) {
-        this._r = r;
-    }
+/**
+ * Create a new Result instance representing a failed computation.
+ */
+export function Err<T, E>(error: E): Result<T, E> {
+    return { type: 'err', error };
+}
 
-    /**
-     * Create a new Result instance representing a successful computation.
-     */
-    static ok(value: T): Result<E, T> {
-        return new Result({ type: Ok, value });
-    }
+export function toString<T, E>(r: Result<T, E>): string {
+    return r.type === 'ok' ? `Ok(${String(r.value)})` : `Err(${String(r.error)})`;
+}
 
-    /**
-     * Create a new Result instance representing a failed computation.
-     */
-    static err(error: E): Result<E, T> {
-        return new Result({ type: Err, error });
-    }
+export function isOk<T, E>(r: Result<T, E>): boolean {
+    return r.type === 'ok';
+}
 
-    toString(): string {
-        const r = this._r;
-        return r.type === Ok ? `Ok(${String(r.value)})` : `Err(${String(r.error)})`;
-    }
+export function isErr<T, E>(r: Result<T, E>): boolean {
+    return r.type === 'err';
+}
 
-    isOk(): boolean {
-        return this._r.type === Ok;
-    }
+export function withDefault<T, E>(r: Result<T, E>, defaultValue: T): T {
+    return r.type === 'ok' ? r.value : defaultValue;
+}
 
-    isErr(): boolean {
-        return this._r.type === Err;
-    }
+export function value<T, E>(r: Result<T, E>): void | T {
+    return r.type === 'ok' ? r.value : undefined;
+}
 
-    withDefault(defaultValue: T): T {
-        const r = this._r;
-        return r.type === Ok ? r.value : defaultValue;
-    }
+export function errValue<T, E>(r: Result<T, E>): void | E {
+    return r.type === 'err' ? r.error : undefined;
+}
 
-    value(): void | T {
-        const r = this._r;
-        return r.type === Ok ? r.value : undefined;
-    }
-
-    errValue(): void | E {
-        const r = this._r;
-        return r.type === Err ? r.error : undefined;
-    }
-
-    /**
-     * Unwrap the value from this Result instance if this is an "Ok" result.
-     * Otherwise, will throw the "Err" error via a runtime exception.
-     */
-    unwrap(): T {
-        const r = this._r;
-        if (r.type === Ok) {
-            return r.value;
-        } else {
-            throw r.error;
-        }
-    }
-
-    expect(message: string | Error): T {
-        const r = this._r;
-        if (r.type === Ok) {
-            return r.value;
-        } else {
-            throw message instanceof Error ? message : new Error(message);
-        }
-    }
-
-    dispatch<O>(okCallback: (T) => O, errCallback: (E) => O): O {
-        const r = this._r;
-        return r.type === Ok ? okCallback(r.value) : errCallback(r.error);
-    }
-
-    /**
-     * Chain together a sequence of computations that may fail.
-     */
-    andThen<V>(callback: (T) => Result<E, V>): Result<E, V> {
-        const r = this._r;
-        return r.type === Ok ? callback(r.value) : Result.err(r.error);
-    }
-
-    /**
-     * Transform an Ok result.  If the result is an Err, the same error value
-     * will propagate through.
-     */
-    map<T2>(mapper: (T) => T2): Result<E, T2> {
-        const r: $ResultT<E, T> = this._r;
-        return r.type === Ok ? Result.ok(mapper(r.value)) : Result.err(r.error);
-    }
-
-    /**
-     * Transform an Err value.  If the result is an Ok, this is a no-op.
-     * Useful when for example the errors has too much information.
-     */
-    mapError<E2>(mapper: (E) => E2): Result<E2, T> {
-        const r = this._r;
-        return r.type === Ok ? Result.ok(r.value) : Result.err(mapper(r.error));
+/**
+ * Unwrap the value from this Result instance if this is an "Ok" result.
+ * Otherwise, will throw the "Err" error via a runtime exception.
+ */
+export function unwrap<T, E>(r: Result<T, E>): T {
+    if (r.type === 'ok') {
+        return r.value;
+    } else {
+        throw r.error;
     }
 }
 
-const _Ok = <E, T>(value: T): Result<E, T> => Result.ok(value);
-const _Err = <E, T>(error: E): Result<E, T> => Result.err(error);
+export function expect<T, E>(r: Result<T, E>, message: string | Error): T {
+    if (r.type === 'ok') {
+        return r.value;
+    } else {
+        throw message instanceof Error ? message : new Error(message);
+    }
+}
 
-// prettier-ignore
-export {
-    Result,
-    _Ok as Ok,
-    _Err as Err,
-};
+export function dispatch<T, E, O>(
+    r: Result<T, E>,
+    okCallback: (T) => O,
+    errCallback: (E) => O,
+): O {
+    return r.type === 'ok' ? okCallback(r.value) : errCallback(r.error);
+}
+
+/**
+ * Chain together a sequence of computations that may fail.
+ */
+export function andThen<T, E, V>(
+    r: Result<T, E>,
+    callback: (T) => Result<V, E>,
+): Result<V, E> {
+    return r.type === 'ok' ? callback(r.value) : Err(r.error);
+}
+
+/**
+ * Transform an Ok result.  If the result is an Err, the same error value
+ * will propagate through.
+ */
+export function map<T, E, T2>(r: Result<T, E>, mapper: (T) => T2): Result<T2, E> {
+    return r.type === 'ok' ? Ok(mapper(r.value)) : Err(r.error);
+}
+
+/**
+ * Transform an Err value.  If the result is an Ok, this is a no-op.
+ * Useful when for example the errors has too much information.
+ */
+export function mapError<T, E, E2>(r: Result<T, E>, mapper: (E) => E2): Result<T, E2> {
+    return r.type === 'ok' ? Ok(r.value) : Err(mapper(r.error));
+}
