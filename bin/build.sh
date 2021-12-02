bfb0c4507253dc651ebc82a8cbf1ee5c7b28e6a9
Vincent Driessen <me@nvie.com>
2021-12-02 12:38:45 +0100

    Screw beauty

--- bin/build.sh
+++ bin/build.sh
@@ -5,9 +5,23 @@ set -e
 ROOT="$(git rev-parse --show-toplevel)"
 SRC="${ROOT}/src"
 DIST="${ROOT}/dist"
-DIST_TYPES="$DIST/ts"
-DIST_CJS="$DIST/cjs"
-DIST_ES="$DIST/es"
+
+#
+# NOTE: Okay, this is quite ugly. Ideally, we'd output to dist/typescript,
+# dist/cjs, and dist/esm separately, but... Flow doesn't seem to be able to
+# correctly look up (or respect) the entrypoint definitions in package.json
+# somehow. If the source files aren't located at the dist root, Flow is unable
+# to find imports using this syntax:
+#
+#   import { ... } from 'decoders/result';
+#                                 ^^^^^^ These submodules won't work in Flow
+#
+# I don't want to sacrifice that API though, so we'll have no choice but to
+# publish the CJS files to the dist root :(
+#
+DIST_CJS="$DIST"
+DIST_ES="$DIST/_esm"
+DIST_TYPES="$DIST/_typescript"
 
 # Work from the project root, independently from where this script is run
 cd "$ROOT"
@@ -35,27 +49,16 @@ copy_typescript_defs() {
 
 copy_flow_defs() {
     flow-copy-source -v -i '**/__tests__/**' -i '**/types/**' "$SRC" "$DIST_CJS"
+    flow-copy-source -v -i '**/__tests__/**' -i '**/types/**' "$SRC" "$DIST_ES"
 }
 
 copy_metadata() {
     cp LICENSE README.md CHANGELOG.md "$DIST"
 }
 
-add_entrypoint() {
-    jq ". + { \
-        main: \"./cjs/index.js\", \
-        module: \"./es/index.js\",
-        exports: { \
-            \"require\": \"./cjs/index.js\", \
-            \"import\": \"./es/index.js\" \
-        },
-        type: \"module\" \
-    }"
-}
-
 add_types_entrypoint() {
     if [ -f "$DIST_TYPES/index.d.ts" ]; then
-        jq '. + { types: "./ts/index.d.ts" }'
+        jq '. + { types: "./_typescript/index.d.ts" }'
     else
         cat  # no-op, pass-thru
     fi
@@ -68,8 +71,9 @@ build_package_json() {
         jq 'del(.importSort)'       | \
         jq 'del(.jest)'             | \
         jq 'del(.scripts)'          | \
+        jq 'del(.type)'             | \
         add_types_entrypoint        | \
-        add_entrypoint                \
+        sed -Ee 's,dist/,,g'          \
         > "$DIST/package.json"
     prettier --write "$DIST/package.json"
 }
