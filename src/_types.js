30bd4b132ca2f887e8a4d18e486840ec26fd5569
Vincent Driessen <me@nvie.com>
2022-01-08 08:42:10 +0100

    Fix comment

--- src/_types.js
+++ src/_types.js
@@ -12,9 +12,7 @@ export type Decoder<T, F = mixed> = (F) => DecodeResult<T>;
 export type Guard<T> = (mixed) => T;
 
 /**
- * A "type function" which informs Flow about how a type will be modified at runtime.
- * Read this as "given a Guard of type T, I can produce a value of type T".  This
- * definition helps construct $ObjMap types.
+ * Returns the output type of a Decoder<T>; in other words: T.
  */
 export type DecoderType = <T>(Decoder<T>) => T;
 export type GuardType = <T>(Guard<T>) => T;
