6a73f2ac861eaea954a317b6145ce424cf4b4dbc
Vincent Driessen <me@nvie.com>
2022-01-08 11:58:54 +0100

    Rename local variables

--- src/result.js
+++ src/result.js
@@ -29,28 +29,28 @@ export function err<E>(error: E): Err<E> {
  * Unwrap the value from this Result instance if this is an "Ok" result.
  * Otherwise, will throw the "Err" error via a runtime exception.
  */
-export function unwrap<T>(result: Result<T, mixed>): T {
-    if (result.ok) {
-        return result.value;
+export function unwrap<T>(r: Result<T, mixed>): T {
+    if (r.ok) {
+        return r.value;
     } else {
-        throw result.error;
+        throw r.error;
     }
 }
 
-export function expect<T>(result: Result<T, mixed>, message: string | Error): T {
-    if (result.ok) {
-        return result.value;
+export function expect<T>(r: Result<T, mixed>, message: string | Error): T {
+    if (r.ok) {
+        return r.value;
     } else {
         throw message instanceof Error ? message : new Error(message);
     }
 }
 
 export function dispatch<T, E, O>(
-    result: Result<T, E>,
+    r: Result<T, E>,
     okCallback: (value: T) => O,
     errCallback: (error: E) => O,
 ): O {
-    return result.ok ? okCallback(result.value) : errCallback(result.error);
+    return r.ok ? okCallback(r.value) : errCallback(r.error);
 }
 
 /**
@@ -58,11 +58,11 @@ export function dispatch<T, E, O>(
  * the first result is an Ok result. If so, it has access to the Ok value from
  * the first argument.
  */
-export function andThen<T, E, T2>(
-    result1: Result<T, E>,
-    lazyResult2: (value: T) => Result<T2, E>,
+export function andThen<T1, E, T2>(
+    r: Result<T1, E>,
+    callback: (value: T1) => Result<T2, E>,
 ): Result<T2, E> {
-    return result1.ok ? lazyResult2(result1.value) : result1;
+    return r.ok ? callback(r.value) : r;
 }
 
 /**
@@ -70,29 +70,26 @@ export function andThen<T, E, T2>(
  * the first result is an Err result. If so, it has access to the Err value
  * from the first argument.
  */
-export function orElse<T, E, E2>(
-    result1: Result<T, E>,
-    lazyResult2: (errValue: E) => Result<T, E2>,
+export function orElse<T, E1, E2>(
+    r: Result<T, E1>,
+    callback: (errValue: E1) => Result<T, E2>,
 ): Result<T, E2> {
-    return result1.ok ? result1 : lazyResult2(result1.error);
+    return r.ok ? r : callback(r.error);
 }
 
 /**
  * Transform an Ok result. Will not touch Err results.
  */
-export function mapOk<T, E, T2>(
-    result: Result<T, E>,
-    mapper: (value: T) => T2,
-): Result<T2, E> {
-    return result.ok ? ok(mapper(result.value)) : result;
+export function mapOk<T, E, T2>(r: Result<T, E>, mapFn: (value: T) => T2): Result<T2, E> {
+    return r.ok ? ok(mapFn(r.value)) : r;
 }
 
 /**
  * Transform an Err value. Will not touch Ok results.
  */
 export function mapError<T, E, E2>(
-    result: Result<T, E>,
-    mapper: (error: E) => E2,
+    r: Result<T, E>,
+    mapFn: (error: E) => E2,
 ): Result<T, E2> {
-    return result.ok ? result : err(mapper(result.error));
+    return r.ok ? r : err(mapFn(r.error));
 }
