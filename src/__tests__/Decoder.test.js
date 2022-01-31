8ef97a2999e325a0fe340e8cbdf903286a0531eb
Vincent Driessen <me@nvie.com>
2022-01-31 10:59:06 +0100

    Reorder tests

--- src/__tests__/Decoder.test.js
+++ src/__tests__/Decoder.test.js
@@ -6,6 +6,39 @@ import { number } from '../lib/numbers';
 import { pojo } from '../lib/objects';
 import { string } from '../lib/strings';
 
+describe('.verify', () => {
+    it('valid', () => {
+        const decoder = number;
+        expect(decoder.verify(0)).toBe(0);
+        expect(decoder.verify(1)).toBe(1);
+        expect(decoder.verify(4)).toBe(4);
+        expect(decoder.verify(-3)).toBe(-3);
+        expect(decoder.verify(-3.14)).toBe(-3.14);
+    });
+
+    it('invalid', () => {
+        const decoder = number;
+        expect(() => decoder.verify('foo')).toThrow('Must be number');
+    });
+
+    it('different erroring styles', () => {
+        const decoder = number;
+
+        // Default
+        expect(() => decoder.verify('xyz')).toThrow('xyz');
+        expect(() => decoder.verify('xyz')).toThrow('Must be number');
+
+        // Same as default
+        expect(() => decoder.verify('xyz', formatInline)).toThrow('xyz');
+        expect(() => decoder.verify('xyz', formatInline)).toThrow('Must be number');
+
+        // Without echoing back the inputs
+        expect(() => decoder.verify('xyz', formatShort)).not.toThrow('xyz');
+        //                                               ^^^ Make sure the input is _NOT_ echoed back
+        expect(() => decoder.verify('xyz', formatShort)).toThrow(/Must be number/);
+    });
+});
+
 describe('.then', () => {
     const hex =
         // We already know how to decode strings...
@@ -123,38 +156,5 @@ describe('.describe', () => {
 
     it('invalid', () => {
         expect(() => decoder.verify(0)).toThrow(/Must be text/);
-    });
-});
-
-describe('.verify', () => {
-    it('valid', () => {
-        const decoder = number;
-        expect(decoder.verify(0)).toBe(0);
-        expect(decoder.verify(1)).toBe(1);
-        expect(decoder.verify(4)).toBe(4);
-        expect(decoder.verify(-3)).toBe(-3);
-        expect(decoder.verify(-3.14)).toBe(-3.14);
-    });
-
-    it('invalid', () => {
-        const decoder = number;
-        expect(() => decoder.verify('foo')).toThrow('Must be number');
-    });
-
-    it('different erroring styles', () => {
-        const decoder = number;
-
-        // Default
-        expect(() => decoder.verify('xyz')).toThrow('xyz');
-        expect(() => decoder.verify('xyz')).toThrow('Must be number');
-
-        // Same as default
-        expect(() => decoder.verify('xyz', formatInline)).toThrow('xyz');
-        expect(() => decoder.verify('xyz', formatInline)).toThrow('Must be number');
-
-        // Without echoing back the inputs
-        expect(() => decoder.verify('xyz', formatShort)).not.toThrow('xyz');
-        //                                               ^^^ Make sure the input is _NOT_ echoed back
-        expect(() => decoder.verify('xyz', formatShort)).toThrow(/Must be number/);
     });
 });
