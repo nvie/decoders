96e2937bdca4fb4c5b1e1105d20198f369c67c95
Vincent Driessen <me@nvie.com>
2022-02-04 10:57:43 +0100

    Make checks more robust

--- bin/check.sh
+++ bin/check.sh
@@ -1,63 +1,60 @@
 #!/bin/sh
 set -eu
 
-# NOTE: This is a poor man's way to check for all exports in src/index.js, but
-# for the time being this will suffice. The more robust solution would be to
-# parse the file and discover all exports that way, but for this this works.
-# Until it no longer works.
+BIN=$(dirname "$0")
+
 list_exports() {
-    cat "$1"                                      \
-    | grep -vEe '^//'                             \
-    | tr -d ';{},\n'                              \
-    | sed -Ee 's/\/\*.*\*\///g' -e "s/'[^']*'//g" \
-    | tr ' ' '\n'                                 \
-    | grep -Ee .                                  \
-    | grep -vEe '(export|from|type)'              \
-    | sort -u
+    "$BIN/linenos" --all --types --exports "$1" | cut -f1 | sort -u
 }
 
 list_decoders() {
-    cat src/index.js                 \
-        | grep -vEe '^//'            \
-        | tr -d '\n'                 \
-        | sed -Ee 's/\/\*.*\*\///g'  \
-        | tr ';' '\n'                \
-        | grep -vEe 'export type'    \
-        | grep -Ee '/core'           \
-        | sed -Ee "s/'[^']*'//g"     \
-        | tr '{}, ' '\n'             \
-        | grep -Ee .                 \
-        | grep -vEe '(export|from)'  \
-        | sort -u
+    "$BIN/linenos" --all --exports src/index.js | cut -f1 | sort -u
 }
 
-tmp1="$(mktemp)"
-tmp2="$(mktemp)"
+tmp1="$(mktemp -t javascript-exports)"
+tmp2="$(mktemp -t typescript-exports)"
 
 list_exports src/index.js > "$tmp1"
 list_exports src/types/index.d.ts > "$tmp2"
 
-if ! diff -q "$tmp1" "$tmp2"; then
-    echo "There were differences between the exports exported from src/index.js and src/types/index.d.ts." >&2
-    echo "Did you forget to update TypeScript definitions?" >&2
+echo "==> Checking parity between TypeScript and Flow" >&2
+if ! diff -q "$tmp1" "$tmp2" 2>/dev/null >/dev/null; then
+    echo "" >&2
+    echo "It looks like there is a difference between the exports for" >&2
+    echo "TypeScript and for Flow!" >&2
+    echo "Please look for differences between the exports in:" >&2
+    echo "- src/index.js" >&2
+    echo "- src/types/index.d.ts" >&2
+    echo "" >&2
+    echo "The diff:" >&2
+    diff -U3 "$tmp1" "$tmp2" >&2
+    echo "" >&2
     exit 2
 fi
 
+echo "==> Checking documentation" >&2
 list_decoders | while read dec; do
-  if ! grep -qEe "\b$dec\b" README.md; then
-      # Cut the either family some slack
-      if [ "$dec" = 'either6' -o  "$dec" = 'either7' -o  "$dec" = 'either8' ]; then
-          continue
-      fi
-
-      echo "Decoder \"$dec\" is not documented in README?" >&2
+  if grep -qEe "\'$dec\': {" docs/_data.py; then
+      continue
+  elif grep -qEe "\'aliases\':.*\'$dec\'" docs/_data.py; then
+      continue
+  else
+      echo "❌ $dec" >&2
+      echo "" >&2
+      echo "It looks like decoder \"$dec\" is not documented yet!" >&2
+      echo "To fix this, please add an entry for it in" >&2
+      echo "" >&2
+      echo "    docs/_data.py" >&2
+      echo "" >&2
       exit 3
   fi
 done
 
+echo "==> Checking type inference tests" >&2
 list_decoders | while read dec; do
   if ! grep -qF "$dec" src/types/tests/typescript-inference-test.ts; then
-      echo "Decoder \"$dec\" is not tested in src/types/tests/typescript-inference-test.ts?" >&2
+      echo "❌ $dec" >&2
+      echo "There is no type inference test for Decoder \"$dec\" in src/types/tests/typescript-inference-test.ts yet." >&2
       exit 4
   fi
 done
