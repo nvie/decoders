f735c4cb72cc9a7c9cd6d42a083968b35e7484b5
Vincent Driessen <me@nvie.com>
2023-01-16 12:51:59 +0100

    Fix TypeScript types for formatter helpers

--- src/types/format.d.ts
+++ src/types/format.d.ts
@@ -2,5 +2,5 @@ import { Annotation } from './annotate';
 
 export type Formatter = (err: Annotation) => string | Error;
 
-export const formatInline: Formatter;
-export const formatShort: Formatter;
+export function formatInline(err: Annotation): string;
+export function formatShort(err: Annotation): string;
