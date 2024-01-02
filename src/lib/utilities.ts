6095a764ebd04afcacad587e213c4465f6d0e4b6
Vincent Driessen <me@nvie.com>
2024-01-02 23:44:58 +0100

    Lint consistency issues

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -4,7 +4,6 @@ import type { Decoder } from '../Decoder';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export interface Klass<T> extends Function {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   new (...args: readonly any[]): T;
 }
 
