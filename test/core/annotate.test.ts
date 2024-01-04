603c03658800a2b1f215d34def07fd96a72f3471
Vincent Driessen <me@nvie.com>
2024-01-04 08:51:33 +0100

    Use maps internally

--- test/core/annotate.test.ts
+++ test/core/annotate.test.ts
@@ -1,41 +1,60 @@
 import { describe, expect, test } from 'vitest';
 
-import { annotate, array, circularRef, merge, object, scalar } from '~/core';
+import {
+  annotate,
+  makeArrayAnn,
+  makeCircularRefAnn,
+  merge as mergeOriginal,
+  makeObjectAnn as makeObjectAnnOriginal,
+  makeScalarAnn,
+} from '~/core/annotate';
+import type { Annotation, ObjectAnnotation } from '~/core/annotate';
+
+function makeObjectAnn(obj: Record<string, Annotation>, text?: string) {
+  return makeObjectAnnOriginal(new Map(Object.entries(obj)), text);
+}
+
+function merge(
+  objAnnotation: ObjectAnnotation,
+  fields: Readonly<Record<string, Annotation>>,
+) {
+  return mergeOriginal(objAnnotation, new Map(Object.entries(fields)));
+}
 
 describe('parsing (scalars)', () => {
   test('strings', () => {
-    expect(annotate('foo')).toEqual(scalar('foo'));
-    expect(annotate('foo', '')).toEqual(scalar('foo', ''));
-    expect(annotate('foo', 'great')).toEqual(scalar('foo', 'great'));
+    expect(annotate('foo')).toEqual(makeScalarAnn('foo'));
+    expect(annotate('foo', '')).toEqual(makeScalarAnn('foo', ''));
+    expect(annotate('foo', 'great')).toEqual(makeScalarAnn('foo', 'great'));
   });
 
   test('booleans', () => {
-    expect(annotate(true)).toEqual(scalar(true));
-    expect(annotate(true, '')).toEqual(scalar(true, ''));
-    expect(annotate(false, 'lies!')).toEqual(scalar(false, 'lies!'));
+    expect(annotate(true)).toEqual(makeScalarAnn(true));
+    expect(annotate(true, '')).toEqual(makeScalarAnn(true, ''));
+    expect(annotate(false, 'lies!')).toEqual(makeScalarAnn(false, 'lies!'));
   });
 
   test('numbers', () => {
-    expect(annotate(123)).toEqual(scalar(123));
-    expect(annotate(234, '')).toEqual(scalar(234, ''));
-    expect(annotate(314, '100x π')).toEqual(scalar(314, '100x π'));
+    expect(annotate(123)).toEqual(makeScalarAnn(123));
+    expect(annotate(234, '')).toEqual(makeScalarAnn(234, ''));
+    expect(annotate(314, '100x π')).toEqual(makeScalarAnn(314, '100x π'));
   });
 
   test('dates', () => {
     const nyd = new Date(2018, 0, 1);
-    expect(annotate(nyd)).toEqual(scalar(nyd));
-    expect(annotate(nyd, '')).toEqual(scalar(nyd, ''));
-    expect(annotate(nyd, "new year's day")).toEqual(scalar(nyd, "new year's day"));
+    expect(annotate(nyd)).toEqual(makeScalarAnn(nyd));
+    expect(annotate(nyd, '')).toEqual(makeScalarAnn(nyd, ''));
+    expect(annotate(nyd, "new year's day")).toEqual(makeScalarAnn(nyd, "new year's day"));
   });
 
   test('null', () => {
-    expect(annotate(null)).toEqual(scalar(null));
-    expect(annotate(null, 'foo')).toEqual(scalar(null, 'foo'));
+    expect(annotate(null)).toEqual(makeScalarAnn(null));
+    expect(annotate(null, 'foo')).toEqual(makeScalarAnn(null, 'foo'));
   });
 
   test('undefined', () => {
-    expect(annotate(undefined)).toEqual(scalar(undefined));
-    expect(annotate(undefined, 'foo')).toEqual(scalar(undefined, 'foo'));
+    expect(annotate(undefined)).toEqual(makeScalarAnn(undefined));
+    expect(annotate(undefined, 'foo')).toEqual(makeScalarAnn(undefined, 'foo'));
   });
 
   test('symbols', () => {
@@ -43,26 +62,30 @@ describe('parsing (scalars)', () => {
     const sym2 = Symbol();
     const sym3 = Symbol('hi');
 
-    expect(annotate(sym1)).toEqual(scalar(sym1));
-    expect(annotate(sym2)).toEqual(scalar(sym2));
-    expect(annotate(sym3)).toEqual(scalar(sym3));
+    expect(annotate(sym1)).toEqual(makeScalarAnn(sym1));
+    expect(annotate(sym2)).toEqual(makeScalarAnn(sym2));
+    expect(annotate(sym3)).toEqual(makeScalarAnn(sym3));
   });
 });
 
 describe('parsing (composite)', () => {
   test('arrays', () => {
     const arr1 = [1, 'foo'];
-    expect(annotate(arr1)).toEqual(array([scalar(1), scalar('foo')]));
+    expect(annotate(arr1)).toEqual(
+      makeArrayAnn([makeScalarAnn(1), makeScalarAnn('foo')]),
+    );
 
     const arr2 = [annotate(1, 'uno'), 'foo'];
-    expect(annotate(arr2)).toEqual(array([scalar(1, 'uno'), scalar('foo')]));
+    expect(annotate(arr2)).toEqual(
+      makeArrayAnn([makeScalarAnn(1, 'uno'), makeScalarAnn('foo')]),
+    );
   });
 
   test('objects', () => {
     const obj = { name: 'Frank' };
     expect(annotate(obj)).toEqual(
-      object({
-        name: scalar('Frank'),
+      makeObjectAnn({
+        name: makeScalarAnn('Frank'),
       }),
     );
   });
@@ -70,39 +93,39 @@ describe('parsing (composite)', () => {
   test('objects (values annotated)', () => {
     const obj = { name: annotate('nvie', 'Vincent'), age: 36 };
     expect(annotate(obj)).toEqual(
-      object({
-        name: scalar('nvie', 'Vincent'),
-        age: scalar(36),
+      makeObjectAnn({
+        name: makeScalarAnn('nvie', 'Vincent'),
+        age: makeScalarAnn(36),
       }),
     );
   });
 
   test('annotates fields in object', () => {
     // Annotate with a simple string
-    const objAnn = object({ name: scalar(null) });
-    expect(merge(objAnn, { name: scalar(null, 'Missing!') })).toEqual(
-      object({
-        name: scalar(null, 'Missing!'),
+    const objAnn = makeObjectAnn({ name: makeScalarAnn(null) });
+    expect(merge(objAnn, { name: makeScalarAnn(null, 'Missing!') })).toEqual(
+      makeObjectAnn({
+        name: makeScalarAnn(null, 'Missing!'),
       }),
     );
 
     // Annotate with a full annotation object (able to change the annotate value itself)
-    const obj2 = object({ name: scalar(null), age: scalar(20) });
-    expect(merge(obj2, { name: scalar(null, 'An example value') })).toEqual(
-      object({
-        name: scalar(null, 'An example value'),
-        age: scalar(20),
+    const obj2 = makeObjectAnn({ name: makeScalarAnn(null), age: makeScalarAnn(20) });
+    expect(merge(obj2, { name: makeScalarAnn(null, 'An example value') })).toEqual(
+      makeObjectAnn({
+        name: makeScalarAnn(null, 'An example value'),
+        age: makeScalarAnn(20),
       }),
     );
   });
 
   test('annotates missing fields in object', () => {
     // Annotate with a simple string
-    const obj = object({ foo: scalar('hello') });
-    expect(merge(obj, { bar: scalar(undefined, 'Missing') })).toEqual(
-      object({
-        foo: scalar('hello'),
-        bar: scalar(undefined, 'Missing'),
+    const obj = makeObjectAnn({ foo: makeScalarAnn('hello') });
+    expect(merge(obj, { bar: makeScalarAnn(undefined, 'Missing') })).toEqual(
+      makeObjectAnn({
+        foo: makeScalarAnn('hello'),
+        bar: makeScalarAnn(undefined, 'Missing'),
       }),
     );
   });
@@ -128,7 +151,10 @@ describe('annotating circular objects', () => {
     const circularArray: any[] = ['foo', [42 /* circular ref will go here */]];
     circularArray[1].push(circularArray);
 
-    const expected = array([scalar('foo'), array([scalar(42), circularRef()])]);
+    const expected = makeArrayAnn([
+      makeScalarAnn('foo'),
+      makeArrayAnn([makeScalarAnn(42), makeCircularRefAnn()]),
+    ]);
 
     expect(annotate(circularArray)).toEqual(expected);
 
@@ -144,13 +170,13 @@ describe('annotating circular objects', () => {
     (circularObject.bar as any).self = circularObject;
     (circularObject as any).self = circularObject;
 
-    const expected = object({
-      foo: scalar(42),
-      bar: object({
-        qux: scalar('hello'),
-        self: circularRef(),
+    const expected = makeObjectAnn({
+      foo: makeScalarAnn(42),
+      bar: makeObjectAnn({
+        qux: makeScalarAnn('hello'),
+        self: makeCircularRefAnn(),
       }),
-      self: circularRef(),
+      self: makeCircularRefAnn(),
     });
 
     expect(annotate(circularObject)).toEqual(expected);
