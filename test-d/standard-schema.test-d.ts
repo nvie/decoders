69d489b96d226868829fee8d2328aba371286b1e
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-01-27 20:36:31 +0100

    Experiment with type tests

---
+++ test-d/standard-schema.test-d.ts
@@ -0,0 +1,16 @@
+import { string } from '../dist';
+import { expectAssignable, expectType } from 'tsd';
+import type { StandardSchemaV1 } from '@standard-schema/spec';
+
+expectAssignable<StandardSchemaV1>(string);
+expectAssignable<StandardSchemaV1<string>>(string);
+expectAssignable<StandardSchemaV1<string, string>>(string);
+expectAssignable<StandardSchemaV1<unknown, string>>(string);
+
+const stringToNumber = string.transform((x) => x.length);
+
+const input = {} as StandardSchemaV1.InferInput<typeof stringToNumber>;
+expectType<string>(input);
+
+const output = {} as StandardSchemaV1.InferOutput<typeof stringToNumber>;
+expectType<number>(output);
