2ed6fe2d097ab86e53ae441405dfcb7e2dbe9baa
Vincent Driessen <me@nvie.com>
2025-02-04 10:14:47 +0100

    Final type-level test tweak

--- test-d/standard-schema.test-d.ts
+++ test-d/standard-schema.test-d.ts
@@ -1,4 +1,4 @@
-import { string } from '../dist';
+import { string, numeric } from '../dist';
 import { expectAssignable, expectError, expectType } from 'tsd';
 import type { StandardSchemaV1 } from '@standard-schema/spec';
 
@@ -6,12 +6,10 @@ expectAssignable<StandardSchemaV1>(string);
 expectAssignable<StandardSchemaV1<unknown>>(string);
 expectAssignable<StandardSchemaV1<unknown, string>>(string);
 
-const stringToNumber = string.transform((x) => x.length);
+declare const input: StandardSchemaV1.InferInput<typeof numeric>;
+declare const output: StandardSchemaV1.InferOutput<typeof numeric>;
 
-const input = {} as StandardSchemaV1.InferInput<typeof stringToNumber>;
 expectType<unknown>(input);
-
-const output = {} as StandardSchemaV1.InferOutput<typeof stringToNumber>;
 expectType<number>(output);
 
 // A generic function that accepts an arbitrary spec-compliant validator
@@ -23,4 +21,5 @@ declare function standardValidate<T extends StandardSchemaV1>(
 // `string` and `stringToNumber` are accepted by `standardValidate`
 expectError(standardValidate(() => "I'm not a standard validator", 42));
 expectType<string>(standardValidate(string, "I'm a string"));
-expectType<number>(standardValidate(stringToNumber, "I'm a string"));
+expectType<number>(standardValidate(numeric, "I'm a string"));
+expectType<number>(standardValidate(numeric, ['not', 'a', 'string']));
