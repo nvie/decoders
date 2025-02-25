1c2d12b4629e2b5c1e9b4b14c149d626ec46a147
Vincent Driessen <me@nvie.com>
2025-02-25 10:43:38 +0100

    Include, don't exclude

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'html'],
-      exclude: ['bin/', 'dist/', '*.cjs', 'test/', 'test-d/'],
+      include: ['src/'],
 
       // Require 100% test coverage
       thresholds: {
