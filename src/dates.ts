75f5ac197e40d601f7f7260eb14ec8c876735e80
Vincent Driessen <me@nvie.com>
2026-03-04 12:11:39 +0100

    Add explicit Decoder<Date> type annotation to flexDate

--- src/dates.ts
+++ src/dates.ts
@@ -41,7 +41,7 @@ export const isoDate: Decoder<Date> = isoDateString.transform((value) => new Dat
  * This is commonly useful to build decoders that can be reused to validate
  * object with Date instances as well as objects coming from JSON payloads.
  */
-export const flexDate = select(unknown, (blob) =>
+export const flexDate: Decoder<Date> = select(unknown, (blob) =>
   typeof blob === 'string'
     ? isoDate
     : isDate(blob)
