87e70ea178b87f1c61bca3e06e752c46da16f52c
Vincent Driessen <me@nvie.com>
2026-03-05 23:20:19 +0100

    Highlight only one TOC item at a time

--- docs/app/docs/[[...slug]]/page.tsx
+++ docs/app/docs/[[...slug]]/page.tsx
@@ -25,7 +25,7 @@ export default async function Page(props: {
   const MDX = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage toc={page.data.toc} full={page.data.full} tableOfContent={{ single: true }}>
       <div className="flex items-start justify-between gap-4">
         <DocsTitle>{page.data.title}</DocsTitle>
         <PageActions
