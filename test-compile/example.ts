fa36fd07f753104bd749cd9482fa4682afaf26be
Vincent Driessen <me@nvie.com>
2026-03-06 12:32:08 +0100

    Adjust example to 2.9 APIs

--- test-compile/example.ts
+++ test-compile/example.ts
@@ -1,25 +1,17 @@
 import {
-  object,
-  number,
-  date,
-  string,
   array,
-  oneOf,
+  between,
+  date,
   nullable,
+  number,
+  object,
+  oneOf,
+  sized,
+  string,
   urlString,
 } from 'decoders';
 
-const stringWithLength = (min: number, max: number) =>
-  string.refine(
-    (value) => value.length >= min && value.length <= max,
-    `string must be between ${min} and ${max} characters`,
-  );
-
-const numberInRange = (min: number, max: number) =>
-  number.refine(
-    (value) => value >= min && value <= max,
-    `number must be between ${min} and ${max}`,
-  );
+const stringWithLength = (min: number, max: number) => sized(string, { min, max });
 
 const imageDecoder = object({
   id: number,
@@ -32,7 +24,7 @@ const imageDecoder = object({
 
 const ratingDecoder = object({
   id: number,
-  stars: numberInRange(1, 5),
+  stars: between(1, 5),
   title: stringWithLength(1, 100),
   text: stringWithLength(1, 1000),
   images: array(imageDecoder),
@@ -44,9 +36,9 @@ const productDecoder = object({
   title: stringWithLength(1, 100),
   brand: stringWithLength(1, 30),
   description: stringWithLength(1, 500),
-  price: numberInRange(1, 10000),
-  discount: nullable(numberInRange(1, 100)),
-  quantity: numberInRange(1, 10),
+  price: between(1, 10000),
+  discount: nullable(between(1, 100)),
+  quantity: between(1, 10),
   tags: array(stringWithLength(1, 30)),
   images: array(imageDecoder),
   ratings: array(ratingDecoder),
