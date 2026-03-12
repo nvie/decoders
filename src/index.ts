e384f8bc33e7bd7b45b689a9e3b03b570821a101
Vincent Driessen <me@nvie.com>
2026-03-12 16:22:00 +0100

    Export Relax<T> type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 // All built-in decoders
-export { array, nonEmptyArray, poja, tuple } from './arrays';
-export { always, anything, constant, fail, never, unknown } from './basics';
-export { null_, nullable, nullish, optional, undefined_ } from './basics';
-export { boolean, truthy } from './booleans';
-export { mapping, record, setFromArray } from './collections';
-export { date, flexDate, iso8601, isoDate, isoDateString } from './dates';
-export { datelike, dateString } from './dates';
-export type { JSONArray, JSONObject, JSONValue } from './json';
-export { json, jsonArray, jsonObject } from './json';
-export type { Sized, SizeOptions } from './lib/size-options';
-export { instanceOf, lazy, prep, sized } from './misc';
+export { array, nonEmptyArray, poja, tuple } from '~/arrays';
+export { always, anything, constant, fail, never, unknown } from '~/basics';
+export { null_, nullable, nullish, optional, undefined_ } from '~/basics';
+export { boolean, truthy } from '~/booleans';
+export { mapping, record, setFromArray } from '~/collections';
+export { date, flexDate, iso8601, isoDate, isoDateString } from '~/dates';
+export { datelike, dateString } from '~/dates';
+export type { JSONArray, JSONObject, JSONValue } from '~/json';
+export { json, jsonArray, jsonObject } from '~/json';
+export { instanceOf, lazy, prep, sized } from '~/misc';
 export {
   anyNumber,
   between,
@@ -19,14 +18,14 @@ export {
   number,
   positiveInteger,
   positiveNumber,
-} from './numbers';
-export { bigint } from './numbers';
-export { exact, inexact, object, pojo } from './objects';
-export { endsWith, nonEmptyString, regex, startsWith, string } from './strings';
-export { identifier, nanoid, uuid, uuidv1, uuidv4 } from './strings';
-export { email, httpsUrl, url, urlString } from './strings';
-export { decimal, hexadecimal, numeric } from './strings';
-export { either, enum_, oneOf, select, taggedUnion } from './unions';
+} from '~/numbers';
+export { bigint } from '~/numbers';
+export { exact, inexact, object, pojo } from '~/objects';
+export { endsWith, nonEmptyString, regex, startsWith, string } from '~/strings';
+export { identifier, nanoid, uuid, uuidv1, uuidv4 } from '~/strings';
+export { email, httpsUrl, url, urlString } from '~/strings';
+export { decimal, hexadecimal, numeric } from '~/strings';
+export { either, enum_, oneOf, select, taggedUnion } from '~/unions';
 
 // Core functionality
 export type {
@@ -44,6 +43,8 @@ export type {
   ScalarAnnotation,
 } from '~/core';
 export { define, err, formatInline, formatShort, isDecoder, ok } from '~/core';
+export type { Relax } from '~/lib/Relax';
+export type { Sized, SizeOptions } from '~/lib/size-options';
 export type { Scalar } from '~/lib/types';
 export { isDate, isPlainObject, isPromiseLike } from '~/lib/utils';
 
